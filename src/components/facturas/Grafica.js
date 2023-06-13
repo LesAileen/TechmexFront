69e7fd28892dd49cc976d0075edb6c2c675e3e33
LesAileen <les96@hotmail.es>
2023-06-13 19:04:09 +0200

    grafica css

--- src/components/facturas/Grafica.js
+++ src/components/facturas/Grafica.js
@@ -8,11 +8,23 @@ const Grafica = ({ datos }) => {
   }));
 
   return (
-    <div className="grafica-container">
+    <div className="grafica" style={{backgroundColor:'white'}}>
       <XYPlot width={1000} height={300} xType="ordinal">
         <VerticalBarSeries data={data} />
-        <XAxis tickLabelAngle={-45} hideTicks />
-        <YAxis />
+        <XAxis
+          tickLabelAngle={-45}
+          hideTicks
+          style={{ line: { stroke: 'black' }, text: { fill: 'black' } }}
+          tickStyle={{ fontSize: '12px', color: 'black' }}
+          title="Clientes"
+          titleStyle={{ fontSize: '14px', color: 'black' }}
+        />
+        <YAxis
+          style={{ line: { stroke: 'black' }, text: { fill: 'black' } }}
+          tickStyle={{ fontSize: '12px', color: 'black' }}
+          title="Precio"
+          titleStyle={{ fontSize: '14px', color: 'black' }}
+        />
       </XYPlot>
     </div>
   );
